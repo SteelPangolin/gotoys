c2508662e903e35e9c43c35db69aece4d32cfcc0
Jeremy Ehrhardt <jeremy@bat-country.us>
2014-01-27 23:58:25 -0800

    regexp, bufio, os/exec demo

--- main/main.go
+++ main/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
     "fmt"
-    "github.com/SteelPangolin/gotoys/filter"
+    "flag"
+    "github.com/SteelPangolin/gotoys/problems"
 )
 
 func main() {
-    pat := []byte("fillory")
-    rep := []byte("further")
-    buf := []byte("fillorygoat")
-    newBuf, err := filter.Filter(pat, rep, buf)
-    fmt.Printf("err = %#v\n", err);
-    fmt.Printf("newBuf = %#v\n", string(newBuf));
+    var err error
+
+    flag.Parse()
+
+    var filter problems.MessageFilter
+    filter, err = problems.NewRegexpFilter([]string{
+        // fragment
+        `missing <!DOCTYPE> declaration`,
+        `inserting implicit <body>`,
+        `inserting missing 'title' element`,
+        // Angular
+        `proprietary attribute "ng-`,
+    })
+    if err != nil {
+        fmt.Printf("Filter init error: %v\n", err)
+        return
+    }
+
+    var tidy problems.Scanner
+    tidy, err = problems.NewTidy()
+    if err != nil {
+        fmt.Printf("Tidy init error: %v\n", err)
+        return
+    }
+
+    for _, path := range flag.Args() {
+        msgs, err := tidy.ScanFile(filter, path)
+        if err != nil {
+            fmt.Printf("Tidy scan error for path %s: %v\n\n", path, err)
+            continue
+        }
+        for _, msg := range msgs {
+            fmt.Printf("%s:%v\n", path, msg)
+        }
+        fmt.Println("")
+    }
 }
