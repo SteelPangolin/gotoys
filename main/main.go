d372cf86fb6a0720142f941c45967777d5691745
Jeremy Ehrhardt <jeremy@bat-country.us>
2014-02-01 23:20:33 -0800

    Primitive HTTP proxy

--- main/main.go
+++ main/main.go
@@ -4,6 +4,8 @@ import (
     "log"
     "net/http"
     "encoding/json"
+    "strings"
+    "strconv"
 //    "github.com/SteelPangolin/gotoys/filter"
 )
 
@@ -13,15 +15,50 @@ func echo(w http.ResponseWriter, r *http.Request) {
     reqJSON, err = json.MarshalIndent(r, "", "    ")
     if err != nil {
         log.Print(err)
+        return
     }
     w.Header().Set("Content-Type", "application/json")
     _, err = w.Write(reqJSON)
     if err != nil {
         log.Print(err)
+        return
+    }
+}
+
+const maxBufLen = 4096
+const upstream = "http://riverpig.lan"
+
+func passthru(w http.ResponseWriter, r *http.Request) {
+    log.Print(r.URL.Path)
+    upstreamURL := strings.Join([]string{upstream, r.URL.Path}, "")
+    resp, err := http.Get(upstreamURL)
+    if err != nil {
+        log.Print(err)
+        return
+    }
+    defer resp.Body.Close()
+
+    contentType := resp.Header.Get("Content-Type")
+    if contentType != "" {
+        w.Header().Set("Content-Type", contentType)
+    }
+
+    if resp.ContentLength != -1 {
+        w.Header().Set("Content-Length", strconv.FormatInt(resp.ContentLength, 10))
+    }
+
+    w.WriteHeader(resp.StatusCode)
+
+    buf := make([]byte, 4096)
+    var n int
+    for err = nil; err == nil; {
+        n, err = resp.Body.Read(buf)
+        w.Write(buf[:n])
     }
 }
 
 func main() {
-    http.HandleFunc("/", echo)
+    http.HandleFunc("/echo", echo)
+    http.HandleFunc("/", passthru)
     http.ListenAndServe(":5000", nil)
 }
