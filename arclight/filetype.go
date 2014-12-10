5531361e6d86292d3280e9171daf72fc9b2e799c
Jeremy Ehrhardt <jeremy@bat-country.us>
2014-12-10 00:25:21 -0800

    Improved MIME type detection

--- arclight/filetype.go
+++ arclight/filetype.go
@@ -1,16 +1,82 @@
 package arclight
 
 import (
+	"errors"
 	"github.com/rakyll/magicmime"
 	"io"
 	"log"
 	"mime"
+	"os"
 	slashpath "path"
+	"strings"
 )
 
+type modeMime struct {
+	mode os.FileMode
+	mime string
+}
+
+// Order is significant: in Go, all CharDevices are also Devices.
+var modeMimes = []modeMime{
+	{
+		mode: os.ModeDir,
+		mime: "inode/directory",
+	},
+	{
+		mode: os.ModeCharDevice,
+		mime: "inode/chardevice",
+	},
+	{
+		mode: os.ModeDevice,
+		mime: "inode/blockdevice",
+	},
+	{
+		mode: os.ModeNamedPipe,
+		mime: "inode/fifo",
+	},
+	{
+		mode: os.ModeSocket,
+		mime: "inode/socket",
+	},
+}
+
+var OctetStream = "application/octet-stream"
+
+var RealFSOnly = errors.New("Must be on a real filesystem")
+
+// ignores symlinks
+// see http://standards.freedesktop.org/shared-mime-info-spec/shared-mime-info-spec-latest.html#idm140625828597376
+func MimeTypeByStat(node VfsNode) (string, error) {
+	osnode, ok := node.(*OsNode) // TODO: is this a legit cast?
+	if !ok {
+		return "", RealFSOnly
+	}
+	mode := osnode.Mode()
+	for _, entry := range modeMimes {
+		if mode&entry.mode == entry.mode {
+			return entry.mime, nil
+		}
+	}
+	return OctetStream, nil
+}
+
 // Detect MIME type using file name extension.
-func MimeTypeByExt(node VfsNode) string {
-	return mime.TypeByExtension(slashpath.Ext(node.Name()))
+func MimeTypeByExt(node VfsNode) (string, error) {
+	mimetype_raw := mime.TypeByExtension(slashpath.Ext(node.Name()))
+
+	mediatype, params, err := mime.ParseMediaType(mimetype_raw)
+	if err != nil {
+		return "", err
+	}
+
+	// mime package lies about text type charsets, so drop them
+	if strings.HasPrefix(mediatype, "text/") {
+		delete(params, "charset")
+	}
+
+	mimetype := mime.FormatMediaType(mediatype, params)
+
+	return mimetype, nil
 }
 
 // Initialized the first time MimeTypeByMagic() is called.
@@ -24,13 +90,13 @@ func MimeTypeByMagic_recover() {
 }
 
 // Detect MIME type using libmagic
-func MimeTypeByMagic(file VfsFile) (string, error) {
+func MimeTypeByMagic(node VfsNode) (string, error) {
 	defer MimeTypeByMagic_recover()
 
 	// init libmagic on first call
 	// TODO: lock? what happens if it gets inited twice?
 	if magic == nil {
-		magic, magic_err = magicmime.New()
+		magic, magic_err = magicmime.New(magicmime.MAGIC_MIME)
 	}
 	if magic_err != nil {
 		return "", magic_err
@@ -38,10 +104,14 @@ func MimeTypeByMagic(file VfsFile) (string, error) {
 
 	var mimetype_raw string
 	var err error
-	osfile, ok := file.(*OsFile)
+	osfile, ok := node.(*OsFile)
 	if ok {
 		mimetype_raw, err = magic.TypeByFile(osfile.Path)
 	} else {
+		file, ok := node.(VfsFile)
+		if !ok {
+			return "", errors.New("not a file, can't look for magic")
+		}
 		// read up to 512 bytes
 		// enough for most MIME sniffers?
 		reader, err := file.Reader()
@@ -50,9 +120,9 @@ func MimeTypeByMagic(file VfsFile) (string, error) {
 		}
 
 		closer, ok := reader.(io.Closer)
-        if ok {
-            defer closer.Close()
-        }
+		if ok {
+			defer closer.Close()
+		}
 
 		buf := make([]byte, 512)
 		n, err := reader.Read(buf)
@@ -72,44 +142,65 @@ func MimeTypeByMagic(file VfsFile) (string, error) {
 		return "", err
 	}
 
+	// We don't care if it's empty, it should still use a standard MIME type.
+	if mediatype == "inode/x-empty" {
+		mediatype = OctetStream
+	}
+
+	// There is no binary charset, even for types that actually have a charset param.
+	// It's an annoying artifact of libmagic.
+	if params["charset"] == "binary" {
+		delete(params, "charset")
+	}
+
 	mimetype := mime.FormatMediaType(mediatype, params)
 
 	return mimetype, nil
 }
 
-func DetectMimeTypes(file VfsFileNode) {
-	mimetype_ext := MimeTypeByExt(file)
+func DetectMimeTypes(node VfsNode) {
+	// this will only assign MIME types to stuff that isn't a regular file
+	mimetype_stat, err := MimeTypeByStat(node)
+	if err == nil && mimetype_stat != OctetStream {
+		node.Attrs()["mimetype"] = mimetype_stat
+		return
+	}
 
-	mimetype_magic, err := MimeTypeByMagic(file)
+	mimetype_ext, err := MimeTypeByExt(node)
 	if err != nil {
-		mimetype_magic = ""
+		mimetype_ext = OctetStream
+	}
+
+	mimetype_magic, err := MimeTypeByMagic(node)
+	if err != nil {
+		mimetype_magic = OctetStream
 	}
 
 	// TODO: resolve conflicts when mediatype is the same but
 	// params are only present on one
 
-	if mimetype_ext == "" && mimetype_magic == "" {
+	if mimetype_ext == OctetStream && mimetype_magic == OctetStream {
 		return
 	}
 
 	if mimetype_ext == mimetype_magic {
-		file.Attrs()["mimetype"] = mimetype_ext
+		node.Attrs()["mimetype"] = mimetype_ext
 		return
 	}
 
-	if mimetype_ext != "" && mimetype_magic != "" {
-		file.Attrs()["mimetype_ext"] = mimetype_ext
-		file.Attrs()["mimetype_magic"] = mimetype_magic
+	if mimetype_ext != OctetStream && mimetype_magic != OctetStream {
+		node.Attrs()["mimetype_ext"] = mimetype_ext
+		node.Attrs()["mimetype_magic"] = mimetype_magic
 		return
 	}
 
-	if mimetype_ext != "" {
-		file.Attrs()["mimetype"] = mimetype_ext
+	if mimetype_ext != OctetStream {
+		node.Attrs()["mimetype"] = mimetype_ext
 		return
 	}
 
-	if mimetype_magic != "" {
-		file.Attrs()["mimetype"] = mimetype_magic
+	if mimetype_magic != OctetStream {
+		node.Attrs()["mimetype"] = mimetype_magic
 		return
 	}
 }
--- main/main.go
+++ main/main.go
@@ -1,43 +1,47 @@
 package main
 
 import (
-    "github.com/SteelPangolin/gotoys/arclight"
-    "fmt"
-    "strings"
-    "os"
+	"flag"
+	"fmt"
+	"github.com/SteelPangolin/gotoys/arclight"
+	"os"
+	"strings"
 )
 
 func list(node arclight.VfsNode, depth int) {
-    node = arclight.Specialize(node)
-    pad := strings.Repeat("  ", depth)
-    fmt.Printf("%s%s\n", pad, node.Name())
+	node = arclight.Specialize(node)
+	pad := strings.Repeat("  ", depth)
+	fmt.Printf("%s%s\n", pad, node.Name())
 
-    pad = strings.Repeat("  ", depth + 1)
+	pad = strings.Repeat("  ", depth+1)
 
-    for k, v := range node.Attrs() {
-        fmt.Printf("%s%s: %#q\n", pad, k, v)
-    }
-    
-    dir, ok := node.(arclight.VfsDir)
-    if ok {
-        children, err := dir.Children()
-        if err != nil {
-            fmt.Printf("%serror: %#q\n", pad, err)
-            return
-        }
-        for _, child := range children {
-            list(child, depth + 1)
-        }
-    }
+	for k, v := range node.Attrs() {
+		fmt.Printf("%s%s: %#q\n", pad, k, v)
+	}
+
+	dir, ok := node.(arclight.VfsDir)
+	if ok {
+		children, err := dir.Children()
+		if err != nil {
+			fmt.Printf("%serror: %#q\n", pad, err)
+			return
+		}
+		for _, child := range children {
+			list(child, depth+1)
+		}
+	}
 }
 
 func main() {
-    path := "/Users/jehrhardt/Downloads"
-    fi, err := os.Stat(path)
-    if err != nil {
-        fmt.Printf("error: %#q\n", err)
-        return
-    }
-    root := arclight.NewOsNode(path, fi)
-    list(root, 0)
+	flag.Parse()
+	path := flag.Arg(0)
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		fmt.Printf("error: %#q\n", err)
+		return
+	}
+
+	root := arclight.NewOsNode(path, fi)
+	list(root, 0)
 }
