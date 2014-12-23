78e1a4005b19d5af5694b2b8baa3d2d3cb064b71
Jeremy Ehrhardt <jeremy@bat-country.us>
2014-12-22 17:23:48 -0800

    DBM-backed word counter

--- main/main.go
+++ main/main.go
@@ -1,35 +1,56 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
 	"flag"
 	"fmt"
-	"github.com/SteelPangolin/gotoys/arclight"
 	"os"
-	"strings"
 )
 
-func list(node arclight.VfsNode, depth int) {
-	//node = arclight.Specialize(node)
-	node.Attrs()["mimetype"] = node.MimeType()
-	pad := strings.Repeat("  ", depth)
-	fmt.Printf("%s%s\n", pad, node.Name())
+import (
+	"github.com/SteelPangolin/gotoys/arclight"
+	"github.com/SteelPangolin/gotoys/dbm"
+)
 
-	pad = strings.Repeat("  ", depth+1)
-
-	for k, v := range node.Attrs() {
-		fmt.Printf("%s%s: %#q\n", pad, k, v)
-	}
-
-	dir, ok := node.(arclight.VfsDir)
-	if ok {
+func findTextFiles(node arclight.VfsNode) {
+	if dir, ok := node.(arclight.VfsDir); ok {
 		children, err := dir.Children()
 		if err != nil {
-			fmt.Printf("%serror: %#q\n", pad, err)
+			fmt.Printf("dir listing error: %v\n", err)
 			return
 		}
 		for _, child := range children {
-			list(child, depth+1)
+			findTextFiles(child)
 		}
+	} else if mediatype, _ := node.MimeType(); mediatype == "text/plain" {
+		if osFile, ok := node.(*arclight.OsFile); ok {
+			fmt.Println(osFile.Path)
+		}
+		wordcount(node.(arclight.VfsFile))
+	}
+}
+
+var Counts map[string]uint64 = make(map[string]uint64)
+var Total uint64 = 0
+
+func wordcount(node arclight.VfsFile) {
+	reader, err := node.Open()
+	if err != nil {
+		fmt.Printf("open error: %v\n", err)
+		return
+	}
+	defer reader.Close()
+
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		word := scanner.Text()
+		Counts[word]++
+		Total++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("scanner error: %v\n", err)
 	}
 }
 
@@ -39,10 +60,26 @@ func main() {
 
 	fi, err := os.Stat(path)
 	if err != nil {
-		fmt.Printf("error: %#q\n", err)
+		fmt.Printf("stat error: %v\n", err)
 		return
 	}
 
 	root := arclight.NewOsNode(path, fi)
-	list(root, 0)
+	findTextFiles(root)
+	fmt.Printf("total: %v\n", Total)
+
+	db, err := dbm.Open("wordcount")
+	defer db.Close()
+	if err != nil {
+		fmt.Printf("DBM open error: %v\n", err)
+		return
+	}
+	valueBuf := make([]byte, 8)
+	for word, count := range Counts {
+		binary.LittleEndian.PutUint64(valueBuf, count)
+		err := db.Insert([]byte(word), valueBuf)
+		if err != nil {
+			fmt.Printf("DBM insert error: %v\n", err)
+		}
+	}
 }
